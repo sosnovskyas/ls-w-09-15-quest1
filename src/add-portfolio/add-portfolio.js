b5b72c5eaa00eb2efcdb5242ca35ec81338033fa
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-10-14 16:37:43 +0300

    markup add-portfolio

---
+++ src/add-portfolio/add-portfolio.js
@@ -0,0 +1,30 @@
+;(function($) {
+
+  // DOM Ready
+  $(function() {
+
+    // Binding a click event
+    // From jQuery v.1.7.0 use .on() instead of .bind()
+    $('.add-portfolio_button').bind('click', function(e) {
+
+      // Prevents the default action to be triggered.
+      e.preventDefault();
+
+      // Triggering bPopup when click event is fired
+      $('.add-portfolio_form').bPopup();
+
+    });
+
+  });
+
+  function getName(str) {
+    if (str.lastIndexOf('\\')) {
+      var i = str.lastIndexOf('\\') + 1;
+    } else {
+      var i = str.lastIndexOf('/') + 1;
+    }
+    var filename = str.slice(i);
+    var uploaded = document.getElementById('fileformlabel');
+    uploaded.innerHTML = filename;
+  }
+})(jQuery);
