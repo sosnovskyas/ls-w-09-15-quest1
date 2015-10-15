e85b68d7b1b811a13d19adfb2cdd685cdc5514f0
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-10-15 22:17:29 +0300

    markup add-portfolio

--- src/add-portfolio/add-portfolio.js
+++ src/add-portfolio/add-portfolio.js
@@ -12,19 +12,17 @@
 
       // Triggering bPopup when click event is fired
       $('.add-portfolio_form').bPopup();
-
     });
-
   });
-
-  function getName(str) {
-    if (str.lastIndexOf('\\')) {
-      var i = str.lastIndexOf('\\') + 1;
-    } else {
-      var i = str.lastIndexOf('/') + 1;
-    }
-    var filename = str.slice(i);
-    var uploaded = document.getElementById('fileformlabel');
-    uploaded.innerHTML = filename;
-  }
 })(jQuery);
+
+function getName(str) {
+  if (str.lastIndexOf('\\')) {
+    var i = str.lastIndexOf('\\') + 1;
+  } else {
+    var i = str.lastIndexOf('/') + 1;
+  }
+  var filename = str.slice(i);
+  var uploaded = document.getElementById('fileformlabel');
+  uploaded.innerHTML = filename;
+}
