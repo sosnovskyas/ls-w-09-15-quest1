af97e0a2b069579664ef91497511ec0067b2544a
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-10-22 13:24:55 +0300

    fix

--- gulp/tasks/dev-js-direct.js
+++ gulp/tasks/dev-js-direct.js
@@ -3,7 +3,6 @@ var config = require('../config').devJsDirect;
 var browserSync  = require('browser-sync');
 
 gulp.task('dev-js-direct', function() {
-  debugger;
   gulp.src(config.src)
     .pipe(gulp.dest(config.dest))
   ;
